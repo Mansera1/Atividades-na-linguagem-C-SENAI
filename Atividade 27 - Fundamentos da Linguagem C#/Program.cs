236f0f447e1154d4ae02dcf14930ff3889831806
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:21:53 -0300

    Entrega de Atividades

---
+++ Atividade 27 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,34 @@
+﻿int idade, anosCategoriaB;
+string possuiCategoriaB, cometeuInfracoes;
+
+Console.WriteLine("Digite sua idade: ");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Voce possui CNH na categoria B? (Sim/Não):");
+
+possuiCategoriaB = Convert.ToString(Console.ReadLine());
+
+if (possuiCategoriaB == "Sim")
+{
+    Console.WriteLine("Há quantos anos você possui a CNH na categoria B?");
+
+    anosCategoriaB = Convert.ToInt16(Console.ReadLine());
+}
+else
+{
+    anosCategoriaB = 0;
+}
+
+Console.WriteLine("Você cometeu infrações graves ou gravíssimas nos últimos 12 meses? (Sim/Não)");
+
+cometeuInfracoes = Convert.ToString(Console.ReadLine());
+
+if (idade >= 21 && anosCategoriaB >= 2 && cometeuInfracoes == "Não")
+{
+    Console.WriteLine("Voce pode tirar a CNH de categoria D");
+}
+else
+{
+    Console.WriteLine("Voce NÃO pode tirar a CNH de categoria C");
+}
