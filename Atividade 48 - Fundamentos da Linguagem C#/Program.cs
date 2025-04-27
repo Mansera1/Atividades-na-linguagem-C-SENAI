4feb0f2c56d001d12d4039c0812b100de65469e1
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 13:52:51 -0300

    Entrega de Atividades

---
+++ Atividade 48 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> numeros = new List<int>();
+int maior = 0;
+int menor = int.MaxValue; ;
+
+Console.WriteLine("Infome 10 numeros:");
+
+for (int i = 0; i < 10; i++)
+{
+    int numero = Convert.ToInt32(Console.ReadLine());
+    numeros.Add(numero);
+}
+
+foreach (int numero in numeros)
+{
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+}
+
+Console.Write("O numero maior é: " + maior + " e o numero menor é: " + menor);
