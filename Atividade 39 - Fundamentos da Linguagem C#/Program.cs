42df4c51078884858ef2a8945ec9109fcde9171d
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:43:59 -0300

    Entrega de Atividades

---
+++ Atividade 39 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,50 @@
+﻿int quilometros;
+double m, km, cm, mm;
+
+Console.WriteLine("Escolha o tipo de conversão de Quilômetros");
+
+Console.WriteLine("1:Converter para Metros");
+
+Console.WriteLine("2:Converter para Centimetros");
+
+Console.WriteLine("3:Converter para Millimetros");
+
+quilometros = int.Parse(Console.ReadLine());
+
+switch (quilometros)
+{
+    case 1:
+        Console.WriteLine("1:Converter para Metros");
+
+        Console.WriteLine("Informe o valor em Quilômetros");
+
+        km = Convert.ToDouble(Console.ReadLine());
+
+        m = km / 0.0010000;
+
+        Console.WriteLine("O valor em metros é " + m);
+        break;
+
+    case 2:
+        Console.WriteLine("2:Converter para Centimetros");
+
+        Console.WriteLine("Informe o valor em Quilômetros:");
+        km = Convert.ToDouble(Console.ReadLine());
+
+        cm = km / 0.000010000;
+
+        Console.WriteLine("O valor em centimetros é " + cm);
+        break;
+
+    case 3:
+        Console.WriteLine("3:Converter para Millimetros");
+
+        Console.WriteLine("Informe o valor em Quilômetros:");
+        km = Convert.ToDouble(Console.ReadLine());
+
+        mm = km / 0.0000010000;
+
+        Console.WriteLine("O valor em milimetros é " + mm);
+        break;
+
+}
