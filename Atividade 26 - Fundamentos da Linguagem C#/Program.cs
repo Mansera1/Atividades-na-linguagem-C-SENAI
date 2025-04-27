be682f15c5dbcda830e631344ff47195b92ee13a
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:19:29 -0300

    Entrega de Atividades

---
+++ Atividade 26 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,13 @@
+﻿int area, b, alt;
+
+Console.WriteLine("Digite a base do retangulo: ");
+
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a altura do retangulo: ");
+
+alt = Convert.ToInt32(Console.ReadLine());
+
+area = b * alt;
+
+Console.WriteLine("A área do retângulo é: " + area);
