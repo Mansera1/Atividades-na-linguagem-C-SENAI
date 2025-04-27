e66e8673cbff4886ddf1f0b57a33212426fb091e
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:36:38 -0300

    Entrega de Atividades

---
+++ Atividade 30 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,11 @@
+﻿int contador = 0;
+
+for (int i = 0; i < 50; i++)
+{
+    if (i % 2 == 0)
+    {
+        contador = contador + 1;
+    }
+}
+
+Console.WriteLine("A quantidade de números pares entre 1 e 50 é:" + contador);
