92f1b5387dab97b22fc2d3ed20a48b7733afddff
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:26:42 -0300

    Entrega de Atividades

---
+++ Atividade 29 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,14 @@
+﻿int idade;
+
+Console.WriteLine("Informe a idade: ");
+
+idade = int.Parse(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.WriteLine("Voce é maior de idade");
+}
+else
+{
+    Console.WriteLine("Voce é menor de idade");
+}
