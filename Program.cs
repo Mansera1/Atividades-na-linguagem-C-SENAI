ecf4717dbf7f9dbcd605fff7dd6ef3495a4add25
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:16:27 -0300

    Entrega de Atividades

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int numero1, numero2, resultado;
+
+Console.WriteLine("Digite o primeiro numero: ");
+
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero: ");
+
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+resultado = numero1 * numero2;
+
+Console.WriteLine("O resultado da muliplicação é" + resultado);
