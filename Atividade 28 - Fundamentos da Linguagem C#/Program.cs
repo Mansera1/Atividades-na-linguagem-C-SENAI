e0ccbfdedcfda5ba5b3ab7d94123af1844ea1996
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:24:23 -0300

    Entrega de Atividades

---
+++ Atividade 28 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,16 @@
+﻿int numero, dobro;
+
+Console.WriteLine(" Escreva um numero:");
+
+numero = int.Parse(Console.ReadLine());
+
+dobro = numero * 2;
+
+if (dobro >= 10 && dobro <= 20)
+{
+    Console.WriteLine("O dobro do numero esta no intervalo de 10 a 20");
+}
+else
+{
+    Console.WriteLine("O dobro do numero não esta no intervalo de 10 a 20");
+}
