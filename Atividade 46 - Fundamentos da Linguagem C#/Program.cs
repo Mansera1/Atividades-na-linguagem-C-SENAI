1a2eb3c94ab72e01c108143e802dc3c9aaac10db
Guilherme Mazzon <gui.massao@outlook.com>
2025-04-27 00:47:15 -0300

    Entrega de Atividade

---
+++ Atividade 46 - Fundamentos da Linguagem C#/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> nomes = new List<string>();
+
+
+Console.WriteLine("Informe 10 nomes:");
+
+for (int i = 0; i < 10; i++)
+{
+    string nome = Console.ReadLine();
+    nomes.Add(nome);
+}
+
+Console.WriteLine("Os nomes foram armazenados:");
+
+foreach (string nome in nomes)
+{
+    Console.Write(nome + ",");
+}
